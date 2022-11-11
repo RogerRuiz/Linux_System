602623ac17721e9e3c3d9d2c650c23a7bbeb67eb
Roger Ruiz <rogerruizeducation@gmail.com>
2022-11-11 09:43:11 +0100

    Simplificación al llamar al fichero .sh desde R

--- app/create_script.R
+++ app/create_script.R
@@ -1,6 +1,6 @@
 name_script <- "hola_mundo"
 my_script <- file(paste0(getwd(), "/", name_script, ".sh"))
 writeLines(c("#!bin/sh", "echo 'Hola, Mundo'"), my_script)
-command_run <- paste0("bash ", getwd(), "/", "hola_mundo", ".sh", " &")
+command_run <- paste0("bash ",getwd(), "/", "hola_mundo", ".sh")
 system(command_run)
 
